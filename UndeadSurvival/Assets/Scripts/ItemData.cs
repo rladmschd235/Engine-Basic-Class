844cb399ac712571fb0f261049f4917e646945d1
은총 <shhshsjsjd4@gmail.com>
2023-07-09 23:44:55 +0900

    2023-07-09
    - 아이템 SO 구현
    - 아이템 SO 데이터 아이템 버튼에 연동

---
+++ UndeadSurvival/Assets/Scripts/ItemData.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item", menuName = "Scriptble Object/ItemData")]
+public class ItemData : ScriptableObject
+{
+    public enum ItemType { Melee, Range, Glove, Shoe, Heal };
+
+    [Header("# Main Info")]
+    public ItemType itemType; // 아이템 종류
+    public int itemId; // 아이템 아이디
+    public string itemName; // 아이템 이름
+    public string itemDesc; // 아이템 설명
+    public Sprite itemIcon; // 아이템 아이콘
+
+    [Header("# Level Data")]
+    public float baseDamage; // 기본 피격 데미지
+    public int baseCount; // 근거리: 기본 생성 개수, 원거리: 기본 관통 횟수
+    public float[] damages;
+    public int[] counts; 
+
+    [Header("# Weapon")]
+    public GameObject projectile; // 프리팹
+}
